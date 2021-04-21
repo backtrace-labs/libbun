98226cb6cef392d828fbea98cf002e145b1c0f3e
Rameez Qurashi <rqurashi@backtrace.io>
2021-04-21 11:06:25 -0400

    Fix compilation issues in lib_unwindstack

--- src/backend/libunwindstack/bun_libunwindstack.cpp
+++ src/backend/libunwindstack/bun_libunwindstack.cpp
@@ -13,25 +13,27 @@
 #include <unwindstack/RegsGetLocal.h>
 #include <unwindstack/Unwinder.h>
 
+static size_t libunwindstack_unwind(void *ctx);
+
 extern "C"
 bun_t *_bun_initialize_libunwindstack(struct bun_config *config)
 {
-    bun_t *handle = calloc(1, sizeof(struct bun_handle));
+    bun_t *handle = static_cast<bun_t *>(calloc(1, sizeof(struct bun_handle)));
 
     if (handle == NULL)
         return NULL;
 
-    handle->unwind_function = libbacktrace_unwind;
+    handle->unwind_function = libunwindstack_unwind;
     handle->unwind_buffer = config->buffer;
     handle->unwind_buffer_size = config->buffer_size;
     handle->arch = config->arch;
     return handle;
 }
 
-size_t libbacktrace_unwind(void *ctx)
+size_t libunwindstack_unwind(void *ctx)
 {
-    struct bun_handle *handle = ctx;
-    struct bun_payload_header *hdr = handle->unwind_buffer;
+    struct bun_handle *handle = static_cast<bun_handle *>(ctx);
+    struct bun_payload_header *hdr = static_cast<bun_payload_header *>(handle->unwind_buffer);
     struct bun_writer_reader *writer;
 
     writer = bun_create_writer(handle->unwind_buffer,
@@ -56,22 +58,18 @@ size_t libbacktrace_unwind(void *ctx)
     };
     unwinder.Unwind();
 
-    bun_frame *frame_ptr = reinterpret_cast<bun_frame *>(static_cast<char *>(dest) +
-        sizeof(bun_payload_header));
-    char *end_of_buffer = static_cast<char *>(dest) + buf_size;
-
     for (const auto &frame : unwinder.frames()) {
         struct bun_frame bun_frame;
         memset(&bun_frame, 0, sizeof(bun_frame));
 
-        bun_frame.addr = pc;
-        bun_frame.symbol = frame.function_name.c_str();
+        bun_frame.addr = frame.pc;
+        bun_frame.symbol = const_cast<char *>(frame.function_name.c_str());
         bun_frame.symbol_length = frame.function_name.size();
-        bun_frame.filename = frame.map_name.c_str();
+        bun_frame.filename = const_cast<char *>(frame.map_name.c_str());
         bun_frame.filename_length = frame.map_name.size();
         bun_frame.line_no = frame.function_offset;
 
-        bun_frame_write(writer, &frame);
+        bun_frame_write(writer, &bun_frame);
     }
     bun_free_writer_reader(writer);
     return hdr->size;
